09b51433f9fc2381b60252a1e6bfb5411283e373
Ana-Maria CĂLINESCU (87527) <ana_maria.calinescu@stud.acs.upb.ro>
2020-04-01 20:56:20 +0300

    Back-end + Front-end

--- my_app/front-end_server.py
+++ my_app/front-end_server.py
@@ -17,6 +17,8 @@ def home():
 
 @app.route('/logout', methods = ['POST', 'GET'])
 def logout():
+    payload = {'clicked':1}
+    r = requests.post('http://server:4001/logout', json=payload)
     return render_template('home.html')
 
 @app.route('/login', methods = ['POST', 'GET'])
@@ -47,22 +49,63 @@ def register():
 
 @app.route('/make_transaction', methods = ['POST', 'GET'])
 def make_transaction():
+    if request.method == "POST":
+        source = request.form['source']
+        destination = request.form['destination']
+        pin = request.form['pin']
+        comm = request.form['comm']
+        ssum = request.form['sum']
+        payload = {'source':source, 'destination':destination, 'pin':pin, 'comm':comm, 'sum':ssum}
+        r = requests.post('http://server:4001/make_transaction', json=payload)
+        return render_template('make_transaction.html', signup=r.text.split(" ")[0], correct=r.text.split(" ")[1], enough_money=r.text.split(" ")[2])
     return render_template('make_transaction.html')
 
 @app.route('/transaction_history', methods = ['POST', 'GET'])
 def transaction_history():
+    if request.method == "POST":
+        iban = request.form['iban']
+        payload = {'iban':iban}
+        r = requests.post('http://server:4001/transaction_history', json=payload).json()
+        if r['correct'] == 1:
+            return render_template('transaction_history.html', flights=r['flights'], signup=r['signup'])
+        return render_template('transaction_history.html', signup=r['signup'], correct=r['correct'])
     return render_template('transaction_history.html')
 
 @app.route('/show_cards', methods = ['POST', 'GET'])
 def show_cards():
+    if request.method == "POST":
+        key = 'iban'
+        if key not in request.form:
+            payload = {'clicked':1}
+            r = requests.post('http://server:4001/show_cards', json=payload).json()
+            return render_template('show_cards.html', flights=r['flights'], signup=r['signup'])
+        else:
+            iban = request.form['iban']
+            ssum = request.form['sum']
+            payload = {'iban':iban, 'sum':ssum}
+            r = requests.post('http://server:4001/add_money', json=payload)
+            return render_template('show_cards.html', signup=r.text.split(" ")[0], correct=r.text.split(" ")[1])
     return render_template('show_cards.html')
 
 @app.route('/change_pin', methods = ['POST', 'GET'])
 def change_pin():
+    if request.method == "POST":
+        iban = request.form['iban']
+        pin = request.form['pin']
+        new_pin = request.form['new_pin']
+        rep_pin = request.form['rep_pin']
+        payload = {'iban':iban, 'pin':pin, 'new_pin':new_pin, 'rep_pin':rep_pin}
+        r = requests.post('http://server:4001/change_pin', json=payload)     
+        return render_template('change_pin.html', value=r.text.split(" ")[0], signup=r.text.split(" ")[1], corrct=r.text.split(" ")[2])   
     return render_template('change_pin.html')
 
 @app.route('/create_card', methods = ['POST', 'GET'])
 def create_card():
+    if request.method == "POST":
+        coin = request.form['coin']
+        payload = {'coin':coin}
+        r = requests.post('http://server:4001/create_card', json=payload)     
+        return render_template('create_card.html', pin=r.text.split(" ")[0], iban=r.text.split(" ")[1], signup=r.text.split(" ")[2])   
     return render_template('create_card.html')
 
 if __name__ == '__main__':
--- server_backend/server_b.py
+++ server_backend/server_b.py
@@ -17,8 +17,19 @@ config = {
 'database': 'idp_banking'
 }
 
+accountno = 0
+
 
 server = Flask(__name__)
+
+@server.route('/logout', methods = ['POST', 'GET'])
+def logout():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        accountno = 0
 
 @server.route('/', methods = ['POST', 'GET'])
 def home():
@@ -26,6 +37,7 @@ def home():
 
 @server.route('/login', methods = ['POST', 'GET'])
 def login():
+    global accountno
     payload = request.get_json(silent=True)
     if not payload:
         return Response(status=400)
@@ -35,34 +47,187 @@ def login():
         func = "SELECT findUser(%s, %s)"
         mycursor.execute(func, (payload['uname'], payload['psw']))
         val = mycursor.fetchone()
-        result = val[0]
+        result = str(val[0])
+        connection.commit()
+        func = "SELECT getAccountNo(%s, %s)"
+        mycursor.execute(func, (payload['uname'], payload['psw']))
+        acc = mycursor.fetchone()
+        accountno = acc[0]
         connection.commit()
         connection.close()
         return result
 
 @server.route('/register', methods = ['POST', 'GET'])
 def register():
+    global accountno
     payload = request.get_json(silent=True)
     if not payload:
         return Response(status=400)
     else:
-        connection = mysql.connector.connect(**config)
-        if payload['psw'] == payload['psw-repeat']:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
+            if payload['psw'] == payload['psw-repeat']:
+                mycursor = connection.cursor()
+                func = "SELECT correctUsername(%s)"
+                mycursor.execute(func, (payload['username'],))
+                correct = mycursor.fetchone()
+                if correct[0] == 0:
+                    mycursor.callproc('insertPersonalData', [payload['username'], payload['last'], payload['first'], payload['address'], payload['number'], payload['email'], ])
+                    connection.commit()
+                    accountno = randint(10000, 99999)
+                    mycursor.callproc('insertUser', [payload['username'], payload['psw'], str(accountno), ])
+                    connection.commit()
+                connection.close()
+                val = "1" + str(correct[0])
+                return val
+            connection.close()
+            return "10"
+        return "00"
+
+@server.route('/create_card', methods = ['POST', 'GET'])
+def create_card():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
             mycursor = connection.cursor()
-            func = "SELECT correctUsername(%s)"
-            mycursor.execute(func, (payload['username'],))
+            pin = str(randint(1000, 9999))
+            iban = "ANABNK" + str(randint(1000000, 9999999))
+            mycursor.callproc('insertCard', [iban, pin, accountno, payload['coin'], ])
+            connection.commit()
+            connection.close()
+            result = str(pin) + " " + str(iban) + " " + "0"
+            return result
+        result = "pin" + " " + "iban" + " " + "1"
+        return result
+
+@server.route('/change_pin', methods = ['POST', 'GET'])
+def change_pin():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            val = 0
+            connection = mysql.connector.connect(**config)
+            mycursor = connection.cursor()
+            func = "SELECT correctTransfer(%s, %s, %s)"
+            mycursor.execute(func, (payload['iban'], accountno ,payload['pin']))
             correct = mycursor.fetchone()
-            if correct[0] == 0:
-                mycursor.callproc('insertPersonalData', [payload['username'], payload['last'], payload['first'], payload['address'], payload['number'], payload['email'], ])
+            if correct[0] == 1:
+                if payload['new_pin'] == payload['rep_pin']:
+                    mycursor = connection.cursor()
+                    mycursor.callproc('changePin', [payload['iban'].replace(" ", ""), payload['pin'], payload['new_pin'], ])
+                    connection.commit()
+                    val = 1
+                connection.close()
+                result = str(val) + " " + str(0) + " " + str(correct[0])
+                return result
+            connection.close()
+            result = str(val) + " " + str(0) + " " + str(correct[0])
+            return result
+        result = "-" + " " + str(1) + " " + "-"
+        return result
+
+@server.route('/show_cards', methods = ['POST', 'GET'])
+def show_cards():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
+            value = []
+            mycursor = connection.cursor()
+            mycursor.callproc('displayCards', [str(accountno), ])
+            connection.commit()
+            for result in mycursor.stored_results():
+                value = result.fetchall()
+            connection.close()
+            result = {'flights':value, 'signup':0}
+            return result
+        result = {'flights': '-', 'signup':1}
+        return result
+
+@server.route('/add_money', methods = ['POST', 'GET'])
+def add_money():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
+            mycursor = connection.cursor()
+            func = "SELECT correctIban(%s, %s)"
+            mycursor.execute(func, (payload['iban'].replace(" ", ""), accountno))
+            correct = mycursor.fetchone()
+            if correct[0] == 1:
+                mycursor = connection.cursor()
+                mycursor.callproc('addMoney', [payload['iban'].replace(" ", ""), str(accountno), payload['sum'], ])
                 connection.commit()
-                accountno = randint(10000, 99999)
-                mycursor.callproc('insertUser', [payload['username'], payload['psw'], str(accountno), ])
+            result = str(0) + " " + str(correct[0])
+            return result
+        result = str(1) + " " + "-"
+        return result
+
+@server.route('/make_transaction', methods = ['POST', 'GET'])
+def make_transaction():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
+            mycursor = connection.cursor()
+            func = "SELECT correctTransfer(%s, %s, %s)"
+            mycursor.execute(func, (payload['source'].replace(" ", ""), accountno ,payload['pin']))
+            correct = mycursor.fetchone()
+            func = "SELECT enoughMoney(%s, %s, %s, %s)"
+            mycursor.execute(func, (payload['source'].replace(" ", ""), accountno ,payload['pin'], payload['sum']))
+            enough_money = mycursor.fetchone()
+            if correct[0] == 1 and enough_money[0] == 1:
+                mycursor.callproc('insertTransaction', [payload['source'].replace(" ", ""), payload['destination'], payload['comm'], payload['sum'], date.today(), ])
                 connection.commit()
             connection.close()
-            val = "1" + str(correct[0])
-            return val
-        connection.close()
-        return "00"
+            result = str(0) + " " + str(correct[0]) + " " + str(enough_money[0])
+            return result
+        result = str(1) + " " + "-" + " " + "-"
+        return result
+
+@server.route('/transaction_history', methods = ['POST', 'GET'])
+def transaction_history():
+    global accountno
+    payload = request.get_json(silent=True)
+    if not payload:
+        return Response(status=400)
+    else:
+        if accountno != 0:
+            connection = mysql.connector.connect(**config)
+            value = []
+            mycursor = connection.cursor()
+            func = "SELECT correctIban(%s, %s)"
+            mycursor.execute(func, (payload['iban'].replace(" ", ""), accountno))
+            correct = mycursor.fetchone()
+            if correct[0] == 1:
+                mycursor.callproc('displayTransactions', [payload['iban'].replace(" ", ""), ])
+                connection.commit()
+                for result in mycursor.stored_results():
+                    value = result.fetchall()
+                connection.close()
+                result = {'flights':value, 'signup':0, 'correct':correct[0]}
+                return result
+            connection.close()
+            result = {'correct':correct[0], 'signup':0}
+            return result
+        result = {'correct':"-", 'signup':1}
+        return result
 
 if __name__ == '__main__':
     server.debug = True
